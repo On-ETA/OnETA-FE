7c65d725f4258d29f558593fc86c3e3e6bd52775
김민서 <kimminseo@gmail.com>
2026-09-15 23:01:58 +0900

    Register web service worker

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ import { registerRootComponent } from "expo";
 import App from "./App";
 
 registerRootComponent(App);
+
+if (
+  typeof window !== "undefined" &&
+  typeof navigator !== "undefined" &&
+  "serviceWorker" in navigator
+) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {
+      // PWA registration is optional; the app should still run normally.
+    });
+  });
+}
